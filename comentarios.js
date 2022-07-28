2188e61dbd4ae884a1039224caa50f0a02edc39f
Samuel Hcp <samuelcarneiro223@gmail.com>
2022-07-28 09:57:47 -0300

    First Commit

---
+++ comentarios.js
@@ -0,0 +1,28 @@
+//Para escrever na tela
+//console.log("Bem vindos ao Starter");
+/* Interpolação
+
+console.log(`
+
+    Samuel ${1 + 1}
+`);*/
+
+// Aula 1
+
+// Comentário normal
+/* Comentário em linha */
+
+// String
+// ABC
+// Usando -> "" - '' - ``´´
+
+// Number
+
+// Boolean
+
+// Undefined vs NULL
+
+// Object
+
+// Array
+
---
+++ scripts.js
@@ -0,0 +1,6 @@
+console.log(`
+
+    Samuel ${1 + 1}
+`);
+
+console.log();
